97e9f41a072c5335964762fba7c28328bb416c6e
Mohid <mohid10587@gmail.com>
2022-03-14 08:20:52 -0400

    About us Page customized

--- pages/about.js
+++ pages/about.js
@@ -31,12 +31,81 @@ export default function Contact() {
 </div>
 
 </div>
+<div style={{textAlign:'center',padding:'80px',backgroundColor:'#ffb3d1'}}>
+<span  style={{fontSize:'30px',fontWeight:'bolder'}}>WE ARE ALWAYS SETTING UP NEW BENCHMARK</span><br></br>
+
+<span style={{fontSize:'19px',fontWeight:'bold'}}>  AND EXCEEDING THE EXPECTATIONS OF OUR CUSTOMERS AND CLIENTS.</span>
+<br></br><br></br>
+
+
+<div style={{textAlign:'justify',fontSize:'20px'}}>  Welcome to  Dream71 Bangladesh Ltd., one of the best software companies in Bangladesh. Welcome to a company where your ideas count, where your determination creates a new world, where your talent and hard-works are rewarded. At Dream71 we always endeavor to lead towards development and creating a flexible, dynamic and cooperative organizational cultural.Dream71 Bangladesh Ltd. is one of the promising software, web application, mobile application & game development companies of Bangladesh which aims at creating a new standard in the era of software technology.</div>
+</div>
 
 
 
-    <div style={{backgroundColor:'Black',color:'white',textAlign:'center' ,padding:'40px 0' ,marginTop:'40px'}}>    
-      <h1>This is a about page</h1> 
-    </div>
+<div style={{padding:'50px 0px' ,backgroundColor:'#adc2eb'}}>
+<div style={{textAlign:'center',padding:'0px 80px'}}>
+<span  style={{fontSize:'30px',fontWeight:'bolder'}}>BRIEF HISTORY</span><br></br>
+
+<div style={{color:'black',fontSize:'25px'}}>_____________</div>
+<br></br><br></br>
+
+
+<div style={{textAlign:'justify',fontSize:'20px'}}>  Dream71 Bangladesh Ltd. is the brainchild of some fresh youth brain. These young minds not only dreamt about something big, they chased it. The birth of Dream71 Bangladesh happened when a bunch of dream seekers planned for something extraordinary in the web and mobile development created a foundation, recruited efficient workers and paved the way for making the dream come true. And the best is yet to come out.</div>
+</div>
+</div>
+
+
+
+<div style={{padding:'50px 0px' ,backgroundColor:'#ffb3d1'}}>
+<div style={{textAlign:'center',padding:'0px 80px'}}>
+<span  style={{fontSize:'30px',fontWeight:'bolder'}}>WHAT WE OFFER</span><br></br>
+<div style={{color:'black',fontSize:'25px'}}>_____________</div>
+
+<br></br><br></br>
+
+
+<div style={{textAlign:'justify' ,fontSize:'20px'}}> We offer excellence in user experience & design, agile engineering and innovation strategy for mobile apps in Bangladesh. In addition to design and development services, We narture innovation to generate and refine disruptive ideas. Going through examination, ideation and clarification, a creative team at Dream71 Bangladesh works with founders to always bring something new in mobile app industry. We provide high-quality web & mobile apps which will certainly be a revolutionary step towards the path of rapidly developing technology sector of Bangladesh. As a result, it has helped us to be one of the top software companies in Bangladesh.</div>
+</div>
+</div>
+
+<div style={{padding:'50px 0px' ,backgroundColor:'#adc2eb'}}>
+<div style={{textAlign:'center',padding:'0px 80px'}}>
+<span  style={{fontSize:'30px',fontWeight:'bolder'}}>WHY DREAM71</span><br></br>
+
+<div style={{color:'black',fontSize:'25px'}}>_____________</div>
+<br></br><br></br>
+
+
+<div style={{textAlign:'justify',fontSize:'20px'}}> Dream71 Bangladesh is certainly the easier one to choose among others because of its passion about productivity, simplicity, and beautiful design in web mobile industry. When You choose Dream71 Bangladesh, you get a dedicated team the collective intelligence, determination and people who have efficiency and credibility in problem solving. You own everything we build, we treat every app like it’s our own – we sweat every detail and aren’t satisfied until it is perfect.</div>
+</div>
+</div>
+
+<div style={{padding:'50px 0px' ,backgroundColor:'#ffb3d1'}}>
+<div style={{textAlign:'center',padding:'0px 80px'}}>
+<span  style={{fontSize:'30px',fontWeight:'bolder'}}>WE GOT SKILLS</span><br></br>
+
+<div style={{color:'black',fontSize:'25px'}}>_____________</div>
+<br></br><br></br>
+
+
+<div style={{textAlign:'justify',fontSize:'20px'}}> One of the most prominent and updated companies which deals with software & IT related issues. Initially, it was founded in 2014 with a view to put a mark in the rapidly growing software & gaming industry of the country. Within a very short time Dream71 Bangladesh has started to draw the attention of the common people with its enhancement in quality services and started growing as one of the Top software companies in Bangladesh.</div>
+</div>
+</div>
+<div className="footer-div" style={{marginTop:'0'}} >
+        <p>Address:<br />
+          House No 16 ( Level 5)
+          Block - A
+          Basundhara R/A, Main Road
+          Dhaka-1229
+          Email: info@dream71.com
+          Mobile:+8801550019966, +8801715091734
+          Phone:+88 09 678 71 1971
+          USA Phone +14158002851</p>
+        <p>&copy; Copyright By<span style={{ fontStyle: 'italic' }}> Abu Sayeed Mandal</span></p>
+      </div>
+
+
       </div>
     )
   }
