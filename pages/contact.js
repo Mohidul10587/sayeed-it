0b875af84ac0977b5c56c2de0ad4d0234d745c73
Mohid <mohid10587@gmail.com>
2022-03-17 03:11:02 +0600

    contact page updated

--- pages/contact.js
+++ pages/contact.js
@@ -7,55 +7,70 @@ import Head from 'next/head'
 
 
 export default function Contact() {
-    return (
-      <div>
-<Head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-</Head>
-<div style={{ backgroundColor: "#5046a1" }}>
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+      </Head>
+      <div style={{ backgroundColor: "#5046a1" }}>
 
 
-<div className="navbar">
-  <div className="navbar-heading">
-    <h1 className="navbar">Sayeed It Park</h1>
+        <div className="navbar">
+          <div className="navbar-heading">
+            <h1 className="navbar">Sayeed It Park</h1>
 
-  </div>
+          </div>
 
-  <div className="navbar-link-container">
-    
-      <div><Link href='/'><a className="navbar-link" href="#">Home</a></Link></div>
-      <div><Link href='/contact'><a className="navbar-link" href="#">Contact us</a></Link></div>
-      <div><Link href='/about'><a className="navbar-link" href="#">About Us</a></Link></div>
-      <div><Link href='/service'><a className="navbar-link" href="#">Services</a></Link></div>
+          <div className="navbar-link-container">
 
-  </div>
+            <div><Link href='/'><a className="navbar-link" href="#">Home</a></Link></div>
+            <div><Link href='/contact'><a className="navbar-link" href="#">Contact us</a></Link></div>
+            <div><Link href='/about'><a className="navbar-link" href="#">About Us</a></Link></div>
+            <div><Link href='/service'><a className="navbar-link" href="#">Services</a></Link></div>
 
-</div>
+          </div>
 
-</div>
+        </div>
 
-<h1 style={{textAlign:'center'}}>GET IN TOUCH WITH US</h1>
-<div style={{color:'black',fontSize:'25px',textAlign:'center'}}>_____________</div>
-
-
-<h3 style={{textAlign:'center'}}>Say Hello ! Don't be shy</h3>
-
-
-
-<div style={{display:'flex',justifyContent:'center'}}>
-<div style={{marginTop:'50px',width:'50%'}} >
-        <p>Address:<br />
-          House No 16 ( Level 5)
-          Block - A
-          Basundhara R/A, Main Road
-          Dhaka-1229
-          Email: info@dream71.com
-          Mobile:+8801550019966, +8801715091734
-          Phone:+88 09 678 71 1971
-          USA Phone +14158002851</p>
-       
       </div>
-</div>
+
+      <h1 style={{ textAlign: 'center' }}>GET IN TOUCH WITH US</h1>
+      <div style={{ color: 'black', fontSize: '25px', textAlign: 'center' }}>_____________</div>
+
+
+      <h3 style={{ textAlign: 'center' }}>Say Hello ! Don't be shy</h3>
+
+      <div>
+        <div style={{ width: '40%', margin: '0 auto', textAlign: 'center' }}>
+          <p style={{ textAlign: 'center' }}>Just drop us a line with some description of what you are looking for and we will take it from there.</p>
+        </div>
+
+        <div style={{ width: '30%', margin: '10px auto' }}>
+
+          <label>Email:</label><p></p>
+          <input type="email" />
+          <p></p>
+          <label>Write Your Massage :</label><p></p>
+          <textarea></textarea>
+
+
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ marginTop: '50px', width: '50%' }} >
+          <p>Address:<br />
+            House No 16 ( Level 5)
+            Block - A
+            Basundhara R/A, Main Road
+            Dhaka-1229
+            Email: info@dream71.com
+            Mobile:+8801550019966, +8801715091734
+            Phone:+88 09 678 71 1971
+            USA Phone +14158002851</p>
+
+        </div>
+      </div>
 
 
 
@@ -65,7 +80,7 @@ export default function Contact() {
 
 
 
-<div className="footer-div" style={{marginTop:'50px'}} >
+      <div className="footer-div" style={{ marginTop: '50px' }} >
         <p>Address:<br />
           House No 16 ( Level 5)
           Block - A
@@ -77,10 +92,10 @@ export default function Contact() {
           USA Phone +14158002851</p>
         <p>&copy; Copyright By<span style={{ fontStyle: 'italic' }}> Abu Sayeed Mandal</span></p>
         <a href="#"> <i class="fab fa-facebook-square"></i></a>
-       <a href="#"> <i class="fab fa-linkedin"></i></a>
-       <a href="#">  <i class="fab fa-twitter-square"></i></a>
+        <a href="#"> <i class="fab fa-linkedin"></i></a>
+        <a href="#">  <i class="fab fa-twitter-square"></i></a>
       </div>
 
-      </div>
-    )
-  }
+    </div>
+  )
+}
